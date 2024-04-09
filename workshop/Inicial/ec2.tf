79f84090c885a0ae0b8167240e6bb93524e51498
Jorge More <jhonatan.more5@hotmail.com>
2024-04-09 00:01:20 -0500

    cambios

---
+++ workshop/Inicial/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_instance" "my_vm" {
+    ami = "ami-0395649fbe870727e"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "ServidorIncial"
+    }
+}
